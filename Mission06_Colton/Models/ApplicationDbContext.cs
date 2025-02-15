22c8e5e5d6aad8589eaf5c8bf0fbea2c96d90183
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-02-14 20:00:39 -0700

    new commit

---
+++ Mission06_Colton/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+namespace Mission06_Colton.Models;
+
+
+{
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Movie> Movies { get; set; }
+}
+}
---
+++ Mission06_Colton/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Colton.Models;
+{
+    public class Movie
+    {
+
+    }
+}
